542428897b8107dd086465485cb95f46378b5dd3
“mubarak-bunza” <mubarakabubakar227@gmail.com>
2021-01-10 16:59:16 +0100

    add navbar toggle func

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-var navbar = document.querySelector('.nav')
+let navbar = document.querySelector('.nav')
 
 window.onscroll = function() {
     if (window.scrollY > 300) {
@@ -7,3 +7,17 @@ window.onscroll = function() {
         navbar.classList.remove('scrolled')
     }
 }
+
+let icon = document.querySelector('#icon')
+let menu_btns = document.querySelector('#menu-btn')
+
+icon.addEventListener('click', function(){
+    
+    if(menu_btns.classList.contains('hide')){
+        menu_btns.classList.remove('hide')
+        menu_btns.classList.add('show')
+    }else{
+        menu_btns.classList.remove('show')
+        menu_btns.classList.add('hide')
+    }
+})
